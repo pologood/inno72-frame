0ad0e1759348afbd08828340227294f9bc675414
zb.zhou <qwer1234>
2018-06-26 13:39:43 +0800

    初始化调整

--- inno72-msg-center/src/test/java/com/point72/msg/center/BaseTest.java
+++ inno72-msg-center/src/test/java/com/point72/msg/center/BaseTest.java
@@ -1,9 +1,11 @@
-package com.inno72.msg.center;
+package com.point72.msg.center;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.inno72.msg.center.MsgCenterApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=MsgCenterApplication.class)
