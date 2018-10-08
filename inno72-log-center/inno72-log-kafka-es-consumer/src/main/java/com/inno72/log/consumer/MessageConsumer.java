5c77b9b03af3fc6dc27df1df17f5a31b9ed607aa
xingang <546657868@qq.com>
2018-10-08 09:57:34 +0800

    修改es消费分区设置

--- inno72-log-center/inno72-log-kafka-es-consumer/src/main/java/com/inno72/log/consumer/MessageConsumer.java
+++ inno72-log-center/inno72-log-kafka-es-consumer/src/main/java/com/inno72/log/consumer/MessageConsumer.java
@@ -2,13 +2,15 @@ package com.inno72.log.consumer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
@@ -27,15 +29,56 @@ public class MessageConsumer {
 	@Resource
 	private SysLogRepository sysLogRepository;
 
-	@KafkaListener(topics = TopicEnum.SYS.topic, containerFactory = "batchFactory")
-	public void onSysMessage(List<String> message) {
-		LOGEGR.info("************************************ {} ************************************", message.size());
+	@KafkaListener(id = "id0", topicPartitions = {@TopicPartition( topic = TopicEnum.SYS.topic,partitions = {"0"})}, containerFactory = "batchFactory")
+	public void onSysMessage0(List<ConsumerRecord<?,?>> records) {
 		List<SysLog> sysLogs = new ArrayList<>();
-		for (String msg : message){
-			sysLogs.add(JSON.parseObject(msg, SysLog.class));
+		for (ConsumerRecord<?, ?> record : records){
+			Optional optional = Optional.ofNullable(record.value());
+			if (optional.isPresent()){
+				sysLogs.add(JSON.parseObject(record.value().toString(), SysLog.class));
+			}
 		}
 		Iterable<SysLog> save = sysLogRepository.save(sysLogs);
-		LOGEGR.info("SYS topic【{}】消费结果 ===> {}",TopicEnum.SYS.topic , JSON.toJSONString(save));
+		LOGEGR.debug("SYS partition 0 - topic【{}】消费结果 ===> {}",TopicEnum.SYS.topic , JSON.toJSONString(save));
 
+	}
+
+	@KafkaListener(id = "id1", topicPartitions = {@TopicPartition( topic = TopicEnum.SYS.topic,partitions = {"1"})}, containerFactory = "batchFactory")
+	public void onSysMessage1(List<ConsumerRecord<?,?>> records) {
+		List<SysLog> sysLogs = new ArrayList<>();
+		for (ConsumerRecord<?, ?> record : records){
+			Optional optional = Optional.ofNullable(record.value());
+			if (optional.isPresent()){
+				sysLogs.add(JSON.parseObject(record.value().toString(), SysLog.class));
+			}
+		}
+		Iterable<SysLog> save = sysLogRepository.save(sysLogs);
+		LOGEGR.debug("SYS partition 1 - topic【{}】消费结果 ===> {}",TopicEnum.SYS.topic , JSON.toJSONString(save));
+	}
+
+	@KafkaListener(id = "id2", topicPartitions = {@TopicPartition( topic = TopicEnum.SYS.topic,partitions = {"2"})}, containerFactory = "batchFactory")
+	public void onSysMessage2(List<ConsumerRecord<?,?>> records) {
+		List<SysLog> sysLogs = new ArrayList<>();
+		for (ConsumerRecord<?, ?> record : records){
+			Optional optional = Optional.ofNullable(record.value());
+			if (optional.isPresent()){
+				sysLogs.add(JSON.parseObject(record.value().toString(), SysLog.class));
+			}
+		}
+		Iterable<SysLog> save = sysLogRepository.save(sysLogs);
+		LOGEGR.debug("SYS partition 2 - topic【{}】消费结果 ===> {}",TopicEnum.SYS.topic , JSON.toJSONString(save));
+	}
+
+	@KafkaListener(id = "id3", topicPartitions = {@TopicPartition( topic = TopicEnum.SYS.topic,partitions = {"3"})}, containerFactory = "batchFactory")
+	public void onSysMessage3(List<ConsumerRecord<?,?>> records) {
+		List<SysLog> sysLogs = new ArrayList<>();
+		for (ConsumerRecord<?, ?> record : records){
+			Optional optional = Optional.ofNullable(record.value());
+			if (optional.isPresent()){
+				sysLogs.add(JSON.parseObject(record.value().toString(), SysLog.class));
+			}
+		}
+		Iterable<SysLog> save = sysLogRepository.save(sysLogs);
+		LOGEGR.debug("SYS partition 3 - topic【{}】消费结果 ===> {}",TopicEnum.SYS.topic , JSON.toJSONString(save));
 	}
 }
