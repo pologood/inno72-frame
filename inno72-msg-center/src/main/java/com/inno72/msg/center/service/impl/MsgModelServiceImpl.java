b86d16e5b45a05fbf8d04e71a8c63bd0e7aa3469
gaoxingang <546657868@qq.com>
2018-11-15 20:13:23 +0800

    处理单独推送的情况

--- inno72-msg-center/src/main/java/com/inno72/msg/center/service/impl/MsgModelServiceImpl.java
+++ inno72-msg-center/src/main/java/com/inno72/msg/center/service/impl/MsgModelServiceImpl.java
@@ -475,8 +475,6 @@ public class MsgModelServiceImpl implements MsgModelService {
 
 		} else if (pushModel.getTemplateType() == TransmissionTemplateType.NOTIFICATION_OPEN_APPLICATION.v()) {
 			logger.info("通知打开应用消息");
-			// IOS系统较事逼
-			logger.info("content is " + content);
 			if (pushModel.getOsType() == OsType.IOS.v() || pushModel.getOsType() == OsType.PRO.v()) {
 				TransmissionTemplate template = generateIOSNotifyTemplate(pushModel.getText(), content,
 						pushModel.getTransmissionType(), pushModel.getSound());
@@ -848,7 +846,6 @@ public class MsgModelServiceImpl implements MsgModelService {
 		payload.addCustomMsg("payload", content);
 		payload.setContentAvailable(1);
 		payload.setSound(sound == null || sound.equals("") ? "default" : sound);
-		logger.info("generateIOSNotifyTemplate text is " + text);
 		payload.setAlertMsg(new APNPayload.SimpleAlertMsg(text));
 		template.setAPNInfo(payload);
 		return template;
